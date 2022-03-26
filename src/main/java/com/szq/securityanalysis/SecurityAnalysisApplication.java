41b2c18045467ea3db99c0d22dee6015622636b6
szq <1300853631@qq.com>
2022-03-26 15:47:33 +0800

    第一次提交代码构建项目

---
+++ src/main/java/com/szq/securityanalysis/SecurityAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.szq.securityanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityAnalysisApplication.class, args);
+    }
+
+}
